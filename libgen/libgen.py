34ee11ef35b80cc21628bcd38d459c52ebbc2058
Tmplt <ttemplate223@gmail.com>
2016-10-05 16:43:47 +0200

    libgen is now a class

--- libgen/libgen.py
+++ libgen/libgen.py
@@ -9,7 +9,7 @@ and extract the data we want in their respective _get_* function.
 
 Common variable descriptions:
     soup: a BeautifulSoup.Resultset which we are gonna parse.
-    html_row: a soup, but of a html table row from libgen.
+    html_row: a soup, but that of a html table row from libgen.
 """
 
 import requests
@@ -28,90 +28,113 @@ class column(IntEnum):
     title = 2
     publisher = 3
     year = 4
-    # skip page count column
+    # skip page count column,
     lang = 6
+    # and file size.
     ext = 8
+    mirrors = 9
 
-# Note that any dictionery key whose value if None
-# will not be added to the URL's query string.
-def _fetch_table(query):
-    """Returns a list of search results."""
+class fetcher(object):
+    items = []
 
-    r = None
-    url = "http://%s/search.php"
+    # Note that any dictionary keys whose value is None
+    # will not be added to the URL's query string.
+    def __init__(self, query):
+        r = None
+        url = "http://%s/search.php"
 
-    for mirror in mirrors:
-        r = requests.get(url % mirror, params=query)
-        if r.status_code == requests.codes.ok:
-            # Don't bother with the other mirrors if we
-            # already got what we wanted.
-            break
+        for mirror in mirrors:
+            r = requests.get(url % mirror, params=query)
+            if r.status_code == requests.codes.ok:
+                # Don't bother with the other mirrors if
+                # we already have what we wanted.
+                break
 
-    # If not 200, raises requests.exceptions.HTTPError
-    # or something else.
-    # TODO: catch this!
-    r.raise_for_status()
+        # If not 200, raises requests.exceptions.HTTPError
+        # (or somethig else?)
+        # TODO: catch this!
+        r.raise_for_status()
 
-    soup = bs(r.text, 'html.parser')
+        soup = bs(r.text, 'html.parser')
 
-    results = []
-    for row in soup.find_all('tr'):
-        # The search result table gives every book an integer ID,
-        # so we only want those table rows.
-        if row.find('td').text.isdigit():
-            #result = item(row)
-            results.append(row)
+        for row in soup.find_all('tr'):
+            # The search result table gives every item an integer ID,
+            # so we only want those rows.
+            if row.find('td').text.isdigit():
+                self.items.append(row)
 
-#       for row in soup.find_all('tr'):
-#           item = item(row)
-#
-#           for item in results:
-#               if item.name = 
+    def _get_column(self, index, column):
+        return self.items[index].find_all('td')[column]
 
-    return results
+    def _get_author(self, index):
+        soup = self._get_column(index, column.author)
 
-def _get_author(html_row):
-    author = _get_column(html_row, column.author)
+        author = soup.text.strip()
+        return author if author else None
 
-    if (author):
-        return author.text.strip()
-    return None
+    def _get_title(self, index):
+        soup = self._get_column(index, column.title)
 
-def _get_title(html_row):
-    soup = _get_column(html_row, column.title)
+        def inner_isdigit(value):
+            try:
+                return value.isdigit()
+            except AttributeError:
+                return False
 
-    def inner_isdigit(value):
-        try:
-            return value.isdigit()
-        except AttributeError:
-            return False
+        # A book which is part of a serie will have the same row index
+        # for the title as a book which isn't part of a series.
+        # What the two books share, however, is that the <a> tag has the
+        # book's ID (an integer) in the "id" attribute.
+        # TODO: can this be simplified by using BeautifulSoup's attributes?
+        soup = soup.find(id=inner_isdigit)
 
-    # A book which is part of a serie will have the same row index
-    # for the title as a book which isn't part of a series.
-    # What the two books share, however, is that the <a> tag has the
-    # book's ID (an integer) in the "id" attribute.
-    # TODO: can this be simplified by using BeautifulSoup's attributes?
-    soup = soup.find(id=inner_isdigit)
+        # This also affects the top-most object, self.items,
+        # and since this strips ISBNs, we should probably extract those,
+        # before running this. Course, we could probably deepcopy it,
+        # but may not be necessary.
+        rmtags = ['br', 'font']
+        for x in soup(rmtags):
+            x.decompose()
 
-    # This also affects the top-most project,
-    # and since this strips ISBNs, we should probably extract those,
-    # before running this.
-    [x.decompose() for x in soup(['br', 'font'])]
+        title = soup.text.strip()
+        return title if title else None
 
-    title = soup.text.strip()
-    return title if title else None
+    def _get_publisher(self, index):
+        soup = self._get_column(index, column.publisher)
 
-def _get_publisher(html_row):
-    pass
+        publisher = soup.text.strip()
+        return publisher if publisher else None
 
-def _get_column(html_row, column):
-    return html_row.find_all('td')[column]
+    def _get_year(self, index):
+        soup = self._get_column(index, column.year)
+
+        year = soup.text.strip()
+        return year if year else None
+
+    def _get_lang(self, index):
+        soup = self._get_column(index, column.lang)
+
+        lang = soup.text.strip()
+        return lang if lang else None
+
+    def _get_ext(self, index):
+        soup = self._get_column(index, column.ext)
+
+        ext = soup.text.strip()
+        return ext if ext else None
+
+    def debugprint(self):
+        print("I found %d result(s)!" % len(self.items))
+        print("The author is:    %s" % self._get_author(0))
+        print("The title is:     %s" % self._get_title(0))
+        print("The publisher is: %s" % self._get_publisher(0))
+        print("The year is:      %s" % self._get_year(0))
+        print("The lang is:      %s" % self._get_lang(0))
+        print("The ext is:       %s" % self._get_ext(0))
 
 def search(query):
     # debugging and testing
     query = {'req': query.title}
-    found = _fetch_table(query)
-    print("I found %d result(s)!" % len(found))
-    print("The author is: %s" % _get_author(found[0]))
-    print("The title is: %s" % _get_title(found[0]))
+    f = fetcher(query)
+    f.debugprint()
 
