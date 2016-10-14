d52c99ceadb770583066f89d1675420039a72f06
Tmplt <ttemplate223@gmail.com>
2016-10-14 22:56:42 +0200

    add missing file
    screw a good-looking git history

---
+++ bookwyrm/utils.py
@@ -0,0 +1,18 @@
+import bencodepy
+import hashlib
+import base64
+
+# Credit to Danilo "DanySK" Pianini
+# <https://github.com/DanySK/torrent2magnet>
+def magnet_from_torrent(torrent):
+    metadata = bencodepy.decode(torrent)
+    subj = metadata[b'info']
+    hashcontents = bencodepy.encode(subj)
+    digest = hashlib.sha1(hashcontents).digest()
+    b32hash = base64.b32encode(digest).decode()
+
+    return 'magnet:?' \
+             + 'xt=urn:btih:' + b32hash \
+             + '&dn=' + metadata[b'info'][b'name'].decode() \
+             + '&tr=' + metadata[b'announce'].decode() \
+             + '&xl=' + str(metadata[b'info'][b'length'])
