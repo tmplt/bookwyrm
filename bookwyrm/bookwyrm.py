14c10f1c6641b0dee90a24102044c96ce7fcf363
Tmplt <ttemplate223@gmail.com>
2016-10-29 19:20:23 +0200

    bookwyrm/print_items: use item.title shorthand

--- bookwyrm/bookwyrm.py
+++ bookwyrm/bookwyrm.py
@@ -53,13 +53,12 @@ class bookwyrm:
 
     def print_items(self):
         for idx, item in enumerate(self.results):
-            title = item.data.title
             edition = item.exacts.edition
             ext = item.exacts.ext
 
             output = "%d | %s, %s, %s" % (
                 idx,
-                title,
+                item.title,
                 utils.ordinal_num(edition) + " ed." if edition else "n/a ed.",
                 ext
             )
