52c00264fdf24af330edaaecabb6ae1495e71a59
Tmplt <ttemplate223@gmail.com>
2016-11-06 00:07:17 +0100

    libgen: become an object

--- bookwyrm/libgen/libgen.py
+++ bookwyrm/libgen/libgen.py
@@ -57,6 +57,184 @@ class column(IntEnum):
     mirrors_end = 12
 
 
+class _Row(object):
+    """
+    Store and retrieve data from a html table row.
+    """
+
+    def __init__(self, row):
+        self.row = row
+
+    def _get_column(self, column):
+        return self.row.find_all('td')[column]
+
+    def _get_authors(self):
+        soup = self._get_column(column.authors)
+
+        delim = r'[,;]'  # NOTE: 'and' is also used, it seems
+        authors = re.split(delim, soup.text.strip())
+
+        # NOTE: can we strip with re.split() instead?
+        authors = [author.strip() for author in authors]
+
+        return authors if authors else None
+
+    def _get_title(self):
+        soup = self._get_column(column.title)
+
+        def inner_isdigit(value):
+            try:
+                return value.isdigit()
+            except AttributeError:
+                return False
+
+        # Book series name, its title, the edition of it and all of its
+        # ISBN numbers share the same column. Luckily, the title's tag will
+        # have an 'id'-attribute containing a digit, differing it from the
+        # other tags in the column; we search for that tag.
+        #
+        # NOTE: the 'id'-attribute only appears where the title text is.
+        #       Perhaps we don't need to verify it is a digit?
+        soup = soup.find(id=inner_isdigit)
+
+        # The found tags are references, so the top-most object will be affected,
+        # and since this strips ISBNs, we will extract those before running this.
+        rmtags = ['br', 'font']
+        for tag in soup(rmtags):
+            tag.decompose()
+
+        title = soup.text.strip()
+        return title if title else None
+
+    def _get_serie(self):
+        soup = self._get_column(column.serie)
+
+        query = soup.a['href']
+        o = urlparse(query)
+        od = parse_qs(o.query)
+
+        try:
+            serie = od.get['req'][0]
+        except (KeyError, TypeError):
+            serie = None
+
+        return serie
+
+    def _get_isbns(self):
+        soup = self._get_column(column.isbns)
+        try:
+            soup = soup.find('br').find('i')
+
+            isbns = soup.text.split(', ')
+            return isbns if isbns else None
+        except AttributeError:  # not all entries will have ISBN numbers
+            return None
+
+    def _get_edition(self):
+        soup = self._get_column(column.edition)
+        soups = soup.find_all('i')
+
+        for soup in soups:
+            try:
+                edition = soup.text
+            except AttributeError:
+                return None
+
+            # Item editions are always incased in brackets,
+            # e.g. '[6ed.]', '[7th Revised Edition]'.
+            if edition[0] != '[':
+                continue
+
+            # We could use substring to get the edition number,
+            # but in the case that the number is more than one digit,
+            # we regex it instead.
+            try:
+                edition = int(re.findall(r'\d+', edition)[0])
+                return edition
+            except (TypeError, ValueError, IndexError):
+                return None
+
+    def _get_publisher(self):
+        soup = self._get_column(column.publisher)
+
+        publisher = soup.text.strip()
+        return publisher if publisher else None
+
+    def _get_year(self):
+        soup = self._get_column(column.year)
+
+        try:
+            year = int(soup.text.strip())
+        except ValueError:
+            return None
+        return year
+
+    def _get_page_count(self):
+        soup = self._get_column(column.pages)
+
+        count = soup.text.strip()
+        return count if count else None
+
+    def _get_lang(self):
+        soup = self._get_column(column.lang)
+
+        lang = soup.text.strip()
+        return lang.lower() if lang else None
+
+    def _get_size(self):
+        """
+        Return size in bytes;
+        e.g., "816 kb" -> "816 * 10^3 / 8"
+        """
+        soup = self._get_column(column.size)
+
+        size = soup.text.strip().split()
+        number = size[0]
+        prefix = size[0][1]  # without the 'b' in e.g. 'kb'
+
+        bytesize = int(number * utils.translate_si_prefix(prefix) / 8)
+        return bytesize
+
+    def _get_ext(self):
+        soup = self._get_column(column.ext)
+
+        ext = soup.text.strip()
+        return ext if ext else None
+
+    def _get_mirrors(self):
+        urls = []
+        for col in range(column.mirrors_start, column.mirrors_end):
+            soup = self._get_column(col)
+            url = soup.find('a')['href']
+            urls.append(url)
+
+        return urls
+
+    def as_item(self):
+        # Existing in the same column as the title -- for which
+        # we must decompose all <i>-tags -- these must be extracted first.
+        edition = self._get_edition()
+        isbns = self._get_isbns()
+
+        data = Data(
+            title = self._get_title(),
+            authors = self._get_authors(),
+            serie = self._get_serie(),
+            publisher = self._get_publisher(),
+            mirrors = self._get_mirrors(),
+            isbns = isbns,
+
+            exacts = Exacts(
+                year = self._get_year(),
+                lang = self._get_lang(),
+                edition = edition,
+                ext = self._get_ext()
+            )
+        )
+
+        return Item(data)
+
+
 class _fetcher(object):
     """
     Retrieve the html table and iterate over its rows.
@@ -115,164 +293,6 @@ class _fetcher(object):
         return result[0]
 
 
-def _get_column(row, column):
-    return row.find_all('td')[column]
-
-
-def _get_authors(row):
-    soup = _get_column(row, column.authors)
-
-    delim = r'[,;]'  # NOTE: 'and' is also used, it seems
-    authors = re.split(delim, soup.text.strip())
-
-    # NOTE: can we strip with re.split() instead?
-    authors = [author.strip() for author in authors]
-
-    return authors if authors else None
-
-
-def _get_title(row):
-    soup = _get_column(row, column.title)
-
-    def inner_isdigit(value):
-        try:
-            return value.isdigit()
-        except AttributeError:
-            return False
-
-    # Book series name, its title, the edition of it and all of its
-    # ISBN numbers share the same column. Luckily, the title's tag will
-    # have an 'id'-attribute containing a digit, differing it from the
-    # other tags in the column; we search for that tag.
-    #
-    # NOTE: the 'id'-attribute only appears where the title text is.
-    #       Perhaps we don't need to verify it is a digit?
-    soup = soup.find(id=inner_isdigit)
-
-    # The found tags are references, so the top-most object will be affected,
-    # and since this strips ISBNs, we will extract those before running this.
-    rmtags = ['br', 'font']
-    for tag in soup(rmtags):
-        tag.decompose()
-
-    title = soup.text.strip()
-    return title if title else None
-
-
-def _get_serie(row):
-    soup = _get_column(row, column.serie)
-
-    query = soup.a['href']
-    o = urlparse(query)
-    od = parse_qs(o.query)
-
-    try:
-        serie = od.get['req'][0]
-    except (KeyError, TypeError):
-        serie = None
-
-    return serie
-
-
-def _get_isbns(row):
-    soup = _get_column(row, column.isbns)
-    try:
-        soup = soup.find('br').find('i')
-
-        isbns = soup.text.split(', ')
-        return isbns if isbns else None
-    except AttributeError:  # not all entries will have ISBN numbers
-        return None
-
-
-def _get_edition(row):
-    soup = _get_column(row, column.edition)
-    soups = soup.find_all('i')
-
-    for soup in soups:
-        try:
-            edition = soup.text
-        except AttributeError:
-            return None
-
-        # Item editions are always incased in brackets,
-        # e.g. '[6ed.]', '[7th Revised Edition]'.
-        if edition[0] != '[':
-            continue
-
-        # We could use substring to get the edition number,
-        # but in the case that the number is more than one digit,
-        # we regex it instead.
-        try:
-            edition = int(re.findall(r'\d+', edition)[0])
-            return edition
-        except (TypeError, ValueError, IndexError):
-            return None
-
-
-def _get_publisher(row):
-    soup = _get_column(row, column.publisher)
-
-    publisher = soup.text.strip()
-    return publisher if publisher else None
-
-
-def _get_year(row):
-    soup = _get_column(row, column.year)
-
-    try:
-        year = int(soup.text.strip())
-    except ValueError:
-        return None
-    return year
-
-
-def _get_page_count(row):
-    soup = _get_column(row, column.pages)
-
-    count = soup.text.strip()
-    return count if count else None
-
-
-def _get_lang(row):
-    soup = _get_column(row, column.lang)
-
-    lang = soup.text.strip()
-    return lang.lower() if lang else None
-
-
-def _get_size(row):
-    """
-    Return size in bytes;
-    e.g., "816 kb" -> "816 * 10^3 / 8"
-    """
-    soup = _get_column(row, column.size)
-
-    size = soup.text.strip().split()
-    number = size[0]
-    prefix = size[0][1]  # without the 'b' in e.g. 'kb'
-
-    bytesize = int(number * utils.translate_si_prefix(prefix) / 8)
-    return bytesize
-
-
-def _get_ext(row):
-    soup = _get_column(row, column.ext)
-
-    ext = soup.text.strip()
-    return ext if ext else None
-
-
-def _get_mirrors(row):
-    urls = []
-    for col in range(column.mirrors_start, column.mirrors_end):
-        soup = _get_column(row, col)
-        url = soup.find('a')['href']
-        urls.append(url)
-
-    return urls
-
-
 def search(query):
     """
     Search libgen for `query` and sort the results into a list of Items.
@@ -286,29 +306,9 @@ def search(query):
 
     items = []
     for result in _fetcher(query):
+        row = _Row(result)
+        item = row.as_item()
 
-        # Existing in the same column as the title -- for which
-        # we must decompose all <i>-tags -- these must be extracted first.
-        edition = _get_edition(result)
-        isbns = _get_isbns(result)
-
-        data = Data(
-            title = _get_title(result),
-            authors = _get_authors(result),
-            serie = _get_serie(result),
-            publisher = _get_publisher(result),
-            mirrors = _get_mirrors(result),
-            isbns = isbns,
-
-            exacts = Exacts(
-                year = _get_year(result),
-                lang = _get_lang(result),
-                edition = edition,
-                ext = _get_ext(result)
-            )
-        )
-
-        item = Item(data)
         items.append(item)
 
     return items
