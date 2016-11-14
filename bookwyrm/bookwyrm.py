2efe058f71c6546b82862648aef6ae57fa9d6d30
Tmplt <ttemplate223@gmail.com>
2016-11-14 16:17:16 +0100

    move libgen mirror processing into libgen module

--- bookwyrm/bookwyrm.py
+++ bookwyrm/bookwyrm.py
@@ -20,10 +20,7 @@ from __future__ import print_function
 import sys
 import argparse
 import requests
-import bencodepy
 from enum import Enum, IntEnum
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urlparse, urlencode
 
 from item import Item
 import libgen
@@ -134,100 +131,6 @@ class bookwyrm:
             return IdentType.doi
         else:
             return None
-
-
-def process_mirrors(urls, source=None):
-    """
-    Parse all mirrors and return URIs which can be downloaded
-    with a single request.
-    """
-
-    mirrors = []
-    for url in urls:
-        if url.startswith('http'):
-            r = requests.get(url)
-            soup = bs(r.text, 'html.parser')
-
-            if "golibgen" in url:
-                # To retrieve an item from golibgen, we must construct a query
-                # which contains the item's unique ID and it's file name.
-                #
-                # Golibgen gives us a <form> which contains all this data.
-                # the uid can be found in the 'hidden' attribute and the file
-                # name in 'hidden0'.
-                #
-                # While the .php-script found in the 'action' attribute seems
-                # to be "noleech1.php" for all items, the name seems temporary,
-                # so we extract that too.
-                #
-                # The <form> looks like the following:
-                # <form name="receive" method="GET" action="noleech1.php">
-                #   <input name="hidden"  type="hidden" value="item-UID">
-                #   <input name="hidden0" type="hidden" value="filename">
-                # </form>
-
-                action_tag = soup.find('form', attrs={'name': 'receive'})
-                action = action_tag['action']
-
-                inputs = soup.find('input', attrs={'name': 'hidden'})
-                uid = inputs['value']  # 'value' of the first <input>
-
-                child = inputs.input
-                filename = child['value']
-
-                params = {
-                    'hidden': uid,
-                    'hidden0': filename
-                }
-                params = urlencode(params)
-
-                # NOTE: HTTP refer(r)er "http://golibgen.io/" required
-                # to GET this.
-                url = ('http://golibgen.io/%s?' % action) + params
-                mirrors.append(url)
-
-                continue
-
-            if "bookzz" in url:
-                # Every item is held within in a <div class="actionsHolder">,
-                # but since this search is for an exact match, we will only
-                # ever get one result.
-                #
-                # The <div> looks like the following:
-                # <div class="actionsHolder">
-                #   <div style="float:left;">
-                #    <a class="ddownload color2 dnthandler" href="..." />
-                #   </div>
-                # </div>
-
-                div = soup.find(attrs={'class': 'actionsHolder'})
-
-                # NOTE: HTTP refer(r)er "http://bookzz.org/" required
-                # to GET this.
-                url = div.div.a['href']
-                mirrors.append(url)
-
-                continue
-
-        elif url.startswith("/ads"):
-            # The relative link contains but one parameter which happens to
-            # be the md5 of the item.
-            # To retrieve the .torrent-file, we only need this.
-            o = urlparse(url)
-            md5 = o.query
-
-            torrent_url = "http://libgen.io/book/index.php?%s&oftorrent=" % md5
-            r = requests.get(torrent_url)
-
-            try:
-                magnet = utils.magnet_from_torrent(r.content)
-                mirrors.append(magnet)
-            except bencodepy.DecodingError:
-                pass
-
-            continue
-
-    return mirrors
 
 
 def parse_command_line(parser):
