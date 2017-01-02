1da63350c7868fa97eec001124656708cbee1f9e
Tmplt <tmplt@dragons.rocks>
2017-01-02 21:44:31 +0100

    add all flags from python/master

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,68 @@
 #include <iostream>
+#include <cstdint>
+#include <exception>
 #include "components/command_line.hpp"
+
+using add_arg = command_line::option;
 
 int
 main(int argc, char *argv[])
 {
     const command_line::options opts{
-        command_line::option("-t", "--test", "A testing flag")
+        add_arg("-h", "--help",      "Display this text and exit "),
+        add_arg("-v", "--version",   "Print version information"),
+        add_arg("-l", "--log",       "Set the logging verbosity (default: WARNING)", "LEVEL",
+                {"error", "warning", "info", "trace"}),
+
+        /* Exclusive arguments; cannot be combined with any other arguments. */
+        add_arg("-d", "--ident",     "Specify an item identification (such as DOI, URL, etc.)"),
+
+        /* Main arguments; at least one of these are required. */
+        /* auto main = command_line::add_group( */
+        /*     "main", "necessarily inclusive arguments; at least one required" */
+        /* ); */
+        add_arg("-a", "--author",    "Specify authors"),
+        add_arg("-t", "--title",     "Specify title"),
+        add_arg("-s", "--serie",     "Specify serie"),
+        add_arg("-p", "--publisher", "Specify publisher"),
+
+        /* Exact data arguments; all are optional. */
+        add_arg("-y", "--year",      "Specify year of release"),
+        add_arg("-L", "--language",  "Specify text language"),
+        add_arg("-e", "--edition",   "Specify item edition"),
+        add_arg("-E", "--extension", "Specify item extension", "EXT",
+                {"epub", "pdf", "djvu"}),
+        add_arg("-i", "--isbn",      "Specify item ISBN"),
     };
 
-    std::string progname = argv[0];
-    std::vector<std::string> args(argv + 1, argv + argc);
+    uint8_t exit_code = EXIT_SUCCESS;
 
-    cliparser::make_type cli = cliparser::make(std::move(progname), std::move(opts));
+    try {
+        /* Parse command line arguments */
+        std::string progname = argv[0];
+        std::vector<std::string> args(argv + 1, argv + argc);
 
-    cli->process_input(args);
+        cliparser::make_type cli = cliparser::make(std::move(progname), std::move(opts));
+        cli->process_input(args);
 
-    if (cli->has("test"))
-        std::cout << "We're testing, alright!" << std::endl;
-    else
-        cli->usage();
+        if (cli->has("help")) {
+            cli->usage();
+            return EXIT_SUCCESS;
+        } else if (cli->has("version")) {
+            /*
+             * TODO:
+             * print_build_info(version_details(args));
+             */
+            return EXIT_SUCCESS;
+        } else if (args.empty()) {
+            cli->usage();
+            return EXIT_FAILURE;
+        }
 
-    return 0;
+    } catch(const std::exception &err) {
+        std::cout << err.what() << std::endl;
+        exit_code = EXIT_FAILURE;
+    }
+
+    return exit_code;
 }
