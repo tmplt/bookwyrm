608677368f5bcc9a94b1c429327d25d1a0964531
Tmplt <tmplt@dragons.rocks>
2017-04-13 16:59:20 +0200

    item: finish porting matching() function

---
+++ include/algorithm.hpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 Tmplt <tmplt@dragons.rocks>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <utility>
+
+#include "common.hpp"
+
+namespace algorithm {
+
+/*
+ * Generates the cartesian product between two vectors.
+ */
+template <class T1, class T2>
+const auto product(const vector<T1> &a, const vector<T2> &b)
+{
+    vector<std::pair<T1, T2>> products;
+
+    for (const auto &first : a) {
+        for (const auto &second : b)
+            products.emplace_back(first, second);
+    }
+
+    return products;
+}
+
+}
--- src/item.cpp
+++ src/item.cpp
@@ -15,15 +15,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <array>
+
+#include "fuzzywuzzy.hpp"
+
 #include "item.hpp"
 #include "utils.hpp"
+#include "common.hpp"
+#include "algorithm.hpp"
 
-using std::vector;
-using std::string;
+static constexpr int fuzzy_min = 75;
 
 /*
  * Returns true if all specified exact values are equal
- * and if all specified non-exact values passed the fuzzy ratio.
+ * and if all specified non-exact values passes the fuzzy ratio.
  */
 bool bookwyrm::item::matches(const item &wanted)
 {
@@ -38,7 +43,41 @@ bool bookwyrm::item::matches(const item &wanted)
             !utils::any_intersection(wanted.misc.isbns, this->misc.isbns))
         return false;
 
-    /* TODO: Check nonexacts and authors. */
+    /* Are we copying the strings here? */
+    const std::array<string, 3> in_result = {this->nonexacts.title,
+                                             this->nonexacts.serie,
+                                             this->nonexacts.publisher},
+                                requested = {wanted.nonexacts.title,
+                                             wanted.nonexacts.serie,
+                                             wanted.nonexacts.publisher};
+
+    for (size_t i = 0; i <= in_result.size(); i++) {
+        if (requested[i].empty()) {
+            /*
+             * partial: useful for course literature that can have some
+             * crazy long titles. Also useful for publishers, because
+             * some entries may not use the full name.
+             */
+            if (fuzz::partial_ratio(in_result[i], requested[i]) < fuzzy_min)
+                return false;
+        }
+    }
+
+    if (!wanted.nonexacts.authors.empty()) {
+        int max_ratio = 0;
+        for (const auto &comb : algorithm::product(this->nonexacts.authors,
+                    wanted.nonexacts.authors)) {
+            /*
+             * From some quick testing, it feels like token_set_ratio
+             * works best here.
+             */
+            int ratio = fuzz::token_set_ratio(comb.first, comb.second);
+            max_ratio = std::max(ratio, max_ratio);
+        }
+
+        if (max_ratio < fuzzy_min)
+            return false;
+    }
 
     return true;
 }
