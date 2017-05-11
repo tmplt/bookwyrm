a33b872b904f27ef151b83e4fda64f781a5d804b
Tmplt <tmplt@dragons.rocks>
2017-05-11 17:44:22 +0200

    merge storage.hpp with item.hpp
    There is no need to have the structs seperated.

--- include/item.hpp
+++ include/item.hpp
@@ -17,21 +17,99 @@
 
 #pragma once
 
-#include <pybind11/pybind11.h>
+#include <array>
 #include <tuple>
 
 #include "common.hpp"
-#include "storage.hpp"
 #include "components/command_line.hpp"
 
-namespace py = pybind11;
-
 namespace bookwyrm {
+
+/* Default value: "this value is empty". */
+enum { empty = -1 };
+
+/*
+ * For --year:
+ *   -y 2157   : list items from 2157 (equal)
+ *   -y =>2157 : list items from 2157 and later (eq_gt)
+ *   -y =<2157 : list items from 2157 and earlier (eq_lt)
+ *   -y >2157  : list items from later than 2157 (gt)
+ *   -y <2157  : list items from earlier than 2157 (lt)
+ */
+enum class year_mod { equal, eq_gt, eq_lt, lt, gt };
+
+struct exacts_t {
+    /*
+     * A POD with added index operator.
+     * Useful in item::matches() where we want to
+     * iterate over these values and check if they match.
+     *
+     * We can then get a field value by name, which we'll
+     * want when printing the stuff out.
+     *
+     * Fields are set to "empty" (-1) during construction.
+     * This makes us able to bool-check (since -1 is false)
+     * whether or not a field is empty or not.
+     */
+    explicit exacts_t(const std::unique_ptr<cliparser> &cli);
+    explicit exacts_t(const std::map<string, int> &dict);
+
+    year_mod ymod;
+
+    int year,
+        edition,
+        ext,   // unused for now
+        volume,
+        number,
+        pages, // missing flag
+        lang;  // unused for now
+
+    constexpr static int size = 7;
+
+    std::array<int, size> store = {
+        year, edition, ext,
+        volume, number, pages
+    };
+
+    int operator[](int i) const
+    {
+        return store[i];
+    }
+};
+
+struct nonexacts_t {
+    /*
+     * As the typename suggests, this POD contains
+     * data that we're not going to match exacly match
+     * exactly with the wanted field. Instead, we use
+     * fuzzy-matching.
+     */
+    explicit nonexacts_t(const std::unique_ptr<cliparser> &cli);
+    explicit nonexacts_t(const std::map<string, string> &dict, const vector<string> &authors);
+
+    vector<string> authors;
+    string title;
+    string serie;
+    string publisher;
+    string journal;
+};
+
+struct misc_t {
+    /*
+     * The POD for everything else and undecided
+     * fields.
+     */
+
+    vector<string> isbns;
+    vector<string> mirrors;
+};
+
 
 class item {
 public:
     explicit item(const std::unique_ptr<cliparser> &cli)
         : nonexacts(cli), exacts(cli) {};
+    /* Construct an item from a pybind11::tuple. */
     explicit item(const std::tuple<nonexacts_t, exacts_t> &tuple)
         : nonexacts(std::get<0>(tuple)), exacts(std::get<1>(tuple)) {};
 
