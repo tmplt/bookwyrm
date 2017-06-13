02f84c100b6e296b255b950272e20289ec47ad2b
Tmplt <tmplt@dragons.rocks>
2017-06-13 23:56:12 +0200

    sources: add a second test source

---
+++ src/sources/testsource2.py
@@ -0,0 +1,20 @@
+import pybookwyrm as bw
+import time
+
+
+def find(wanted, bookwyrm):
+    # Changes to wanted are module-local
+    # since the item is copied.
+    wanted.nonexacts.title = "new title"
+
+    # Generate some dummy data
+    for i in range(10):
+        time.sleep(0.2)
+        nonexacts = bw.nonexacts_t(
+            {'serie': 'serie' + str(i), 'title': '2title' + str(i)},
+            ['Author A. ' + str(i), 'Author B.' + str(i)])
+
+        exacts = bw.exacts_t({'year': 2000 + i, 'pages': 500 + i})
+
+        book = (nonexacts, exacts)
+        bookwyrm.feed(book)
