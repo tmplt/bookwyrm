0d73472a0cc17a5feb87db43c895b09d79bdbc26
Tmplt <tmplt@dragons.rocks>
2018-05-03 16:30:49 +0200

    cmake: allow only GNU compiler for now

--- cmake/build/core.cmake
+++ cmake/build/core.cmake
@@ -36,26 +36,10 @@ set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g2")
 
 # Check compiler
-# TODO: update the required compiler versions.
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.0.0")
-    message_colored(FATAL_ERROR "Compiler not supported (Requires clang-5.0.0+ or gcc-5.1+)" 31)
-  else()
-    message_colored(STATUS "Using supported compiler ${CMAKE_CXX_COMPILER_ID}-${CMAKE_CXX_COMPILER_VERSION}" 32)
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wno-nested-anon-types")  # Used by pybind11
-
-    message_colored(STATUS "Linking against libc++ because of LLVM bug <https://bugs.llvm.org/show_bug.cgi?id=33222>" 33)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++ -lc++abi")
-  endif()
-elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.1.0")
-    message_colored(FATAL_ERROR "Compiler not supported (Requires clang-3.4+ or gcc-5.1+)" 31)
-  else()
-    message_colored(STATUS "Using supported compiler ${CMAKE_CXX_COMPILER_ID}-${CMAKE_CXX_COMPILER_VERSION}" 32)
-  endif()
+if(CMAKE_COMPILER_IS_GNUXX)
+    message_colored(FATAL_ERROR "Compiler ${CMAKE_CXX_COMPILER_ID} not supported (only gcc is supported currently; see <https://github.com/Tmplt/bookwyrm/issues/51>.)" 31)
 else()
-  message_colored(WARNING "Using unsupported compiler ${CMAKE_CXX_COMPILER_ID}-${CMAKE_CXX_COMPILER_VERSION} !" 31)
+    message_colored(STATUS "Using ${CMAKE_CXX_COMPILER_ID} compiler." 33)
 endif()
 
 if(ENABLE_CCACHE)
