92cd41b633054da1b67ed3004c9998ed36a063d1
Tmplt <tmplt@dragons.rocks>
2018-05-11 23:17:40 +0200

    add forgotten hpp.cmake file

---
+++ src/core/prefix.hpp.cmake
@@ -0,0 +1,10 @@
+/*
+ * A pre-generation header (or post, if this is config.hpp),
+ * used by cmake to create an up-to-date version string.
+ */
+
+#pragma once
+
+#define INSTALL_PREFIX "@CMAKE_INSTALL_PREFIX@"
+
+// vim: ft=cpp
