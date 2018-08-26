e4556cb6cc1ebc9f27316f2225aa8ee28d6251a7
Tmplt <tmplt@dragons.rocks>
2018-08-26 23:25:48 +0200

    tests: add debug-logging.py and importlib.py

---
+++ tests/plugins/debug-logging.py
@@ -0,0 +1,6 @@
+import pybookwyrm
+
+def find(wanted, bookwyrm):
+    bookwyrm.log.debug('this is a debug log entry')
+
+#PASS debug: this is a debug log entry
---
+++ tests/plugins/importlib.py
@@ -0,0 +1,1 @@
+#PASS cannot load plugin that shares name with a standard library module
