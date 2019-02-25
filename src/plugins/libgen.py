ad296d18489002972ac92b248c785d562d1ddf6a
Tmplt <tmplt@dragons.rocks>
2019-02-25 02:16:50 +0100

    chore(plugin/libgen): don't set item['origin_plugin']
    This field is already set during feed in binding code. See
    `src/core/bindings/python.cpp`.

--- src/plugins/libgen.py
+++ src/plugins/libgen.py
@@ -98,8 +98,6 @@ class LibgenSeeker(object):
         """
         Feed an item to the bookwyrm instance, otherwise print it out.
         """
-        item = {**item, 'origin_plugin': 'libgen.py'}
-
         if self.bookwyrm:
             self.bookwyrm.feed(item)
         else:
